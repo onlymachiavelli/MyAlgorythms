bc02a5775d3981b54eb46f7885743f544d7f93c9
onlymachiavelli <tmakaveli643@gmail.com>
2021-11-17 07:21:49 +0100

    navbar

--- src/UI/Home/navbar.tsx
+++ src/UI/Home/navbar.tsx
@@ -1,12 +1,22 @@
 import React , {} from 'react'
 import styles from './../../../styles/Home.module.css'
 import Link from 'next/link'
+import { Logo } from '../SVG'
 const Navbar = () =>{
     return(
         <nav className = {styles.navbar}>
-            <div className = {styles.logo}></div>
+            <div className = {styles.logo}>
+                <Link href="/">
+                    <a>
+                    <Logo Width="200" Height="50"  />
+                    </a>
+                </Link>
+            </div>
             <nav className={styles.nav}>
-
+                <Link href="">Home</Link>
+                <Link href="">Home</Link>
+                <Link href="">Home</Link>
+                <Link href="">Home</Link>
             </nav>
         </nav>
     )
