7cba6b9f47192d2324c85624bbd9d253fa1e04d0
onlymachiavelli <tmakaveli643@gmail.com>
2021-12-11 15:14:39 +0100

    main slide

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import React from "react"
 import styles from './../styles/Home.module.css'
 import Navbar from "../src/UI/Home/navbar"
 import Head from 'next/head'
+import Image from 'next/image'
+import Main from "../src/UI/Home/slide"
 const Home = () => {
   return (
     <main>
       <header>
         <Navbar/>
       </header>
+      <Main/>
     </main>
   )
 }
---
+++ src/UI/Home/slide.tsx
@@ -0,0 +1,13 @@
+import React, {} from 'react'
+
+import styles from './../../../styles/Home.module.css'
+
+const Main = () =>{
+    return (
+        <div className={styles.main}>
+            <div className={styles.shadow}></div>
+        </div>
+    )
+}
+
+export default Main
